a39c47976baafbb6d39502b7b98b7ac9385ce89e
EminliAysun05 <aeminli232@gmail.com>
2024-05-19 22:57:58 +0400

    Added "find currency rate by code"

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Currency_Rate_system
+﻿using System.ComponentModel.Design;
+
+namespace Currency_Rate_system
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            string[] CurrencyCodes = { "USD","EUR", "AUD", "ARS", "BYN", "BRL", "AED" };
+
+            string[] CurrencyCodes = { "USD", "EUR", "AUD", "ARS", "BYN", "BRL", "AED" };
             decimal[] CurrencyRates = { 1.7000M, 1.8455M, 1.1325M, 0.0019M, 0.5195M, 0.3314M, 0.4628M };
 
             while (true)
@@ -27,8 +29,32 @@
                     }
 
                 }
+                else if (command == "Find currency rate by code")
+                {
+                    Console.WriteLine("Enter code of currency: ");
+                    string codeOfCurrency = Console.ReadLine().ToUpper();
+                    int i = 0;
+                    bool commandOfCurrency = false;
+                    while (i < CurrencyCodes.Length)
+                    {
+                        if (CurrencyCodes[i] == codeOfCurrency )
+                        {
+                            Console.WriteLine($"{codeOfCurrency}: {CurrencyRates[i]}");
+                            break;
+                        }
+                        i++;
+                        
+                    }
+                    if (!commandOfCurrency)
+                    {
+                        Console.WriteLine($"{codeOfCurrency} not found on system. " );
+                    }
 
-           
+
+                }
+
+
+            }
         }
     }
 }
