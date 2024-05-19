3a501787890a89406266cb17f52d7b9bbffaded3
EminliAysun05 <aeminli232@gmail.com>
2024-05-20 01:16:21 +0400

    Added "Find currency rate by code"

--- Program.cs
+++ Program.cs
@@ -9,13 +9,15 @@ namespace Currency_Rate_system
 
             string[] CurrencyCodes = { "USD", "EUR", "AUD", "ARS", "BYN", "BRL", "AED" };
             decimal[] CurrencyRates = { 1.7000M, 1.8455M, 1.1325M, 0.0019M, 0.5195M, 0.3314M, 0.4628M };
+            bool runningOfSystem = true;
 
-            while (true)
+            while (runningOfSystem)
             {
                 Console.WriteLine("Current commands");
                 Console.WriteLine("Show recent currency rates");
                 Console.WriteLine("Find currency rate by code");
                 Console.WriteLine("Calculate amount by currecy");
+                Console.WriteLine("exit");
                 Console.WriteLine("Enter one command: ");
                 string command = Console.ReadLine();
                 if (command == "Show recent currency rates")
@@ -33,24 +35,64 @@ namespace Currency_Rate_system
                 {
                     Console.WriteLine("Enter code of currency: ");
                     string codeOfCurrency = Console.ReadLine().ToUpper();
+                    bool founded = false;
                     int i = 0;
-                    bool commandOfCurrency = false;
+                    
                     while (i < CurrencyCodes.Length)
                     {
-                        if (CurrencyCodes[i] == codeOfCurrency )
+                        if (CurrencyCodes[i] == codeOfCurrency)
                         {
                             Console.WriteLine($"{codeOfCurrency}: {CurrencyRates[i]}");
+                            founded = true;
                             break;
                         }
                         i++;
-                        
+
                     }
-                    if (!commandOfCurrency)
+                    if (!founded)
                     {
-                        Console.WriteLine($"{codeOfCurrency} not found on system. " );
+                        Console.WriteLine($"{codeOfCurrency} not found on system. ");
                     }
-
-
+                }
+                else if(command == "Calculate amount by currecy")
+                {
+                    Console.WriteLine("enter amount: ");
+                    int amount = int.Parse(Console.ReadLine());
+                    if (amount>0)
+                    {
+                        Console.WriteLine("enter code of currency");
+                        string codeOfCurrency = Console.ReadLine().ToUpper();
+                        int i = 0;
+                        bool founded = false;
+                        while(i< CurrencyCodes.Length)
+                        {
+                            if (CurrencyCodes[i] == codeOfCurrency)
+                            {
+                                decimal rate = CurrencyRates[i];
+                                decimal convertAmount = rate*amount;
+                                Console.WriteLine( convertAmount + "AZN");
+                                founded = true;
+                                break;
+                            }
+                            i++;
+                        }
+                        if (!founded)
+                        {
+                            Console.WriteLine("currency not founded");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("entered false amount");
+                    }
+                }
+                else if(command == "exit")
+                {
+                    runningOfSystem = false;
+                }
+                else
+                {
+                    Console.WriteLine("entered wrong command");
                 }
 
 
@@ -58,3 +100,5 @@ namespace Currency_Rate_system
         }
     }
 }
+    
+
