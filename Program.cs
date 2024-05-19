4d96238f750cffd76c0d42c1e79813958e8c7ca3
EminliAysun05 <aeminli232@gmail.com>
2024-05-19 17:26:16 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Currency_Rate_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //777
+            string[] CurrencyCodes = { "USD","EUR", "AUD", "ARS", "BYN", "BRL", "AED" };
+            decimal[] CurrencyRates = { 1.7000M, 1.8455M, 1.1325M, 0.0019M, 0.5195M, 0.3314M, 0.4628M };
+            
+            while (true)
+            {
+                Console.WriteLine("Current commands");
+                Console.WriteLine("Show recent currency rates");
+                Console.WriteLine("Find currency rate by code");
+                Console.WriteLine("Calculate amount by currecy");
+                Console.WriteLine("Enter one command: ");
+                string command = Console.ReadLine();
+                if (command == "Show recent currency rates")
+                {
+                    Console.WriteLine("Show recent currency rates are: ");
+                    int i = 0;
+                    while (i< CurrencyCodes.Length)
+                    {
+                        Console.WriteLine($"{CurrencyCodes[i]}: {CurrencyRates[i]}");
+                        i++;
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
